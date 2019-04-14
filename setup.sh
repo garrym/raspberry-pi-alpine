5829461a432e5d2d1bde2e649f3d9f6c8ae739aa
Garry Marsland <garry@basefour.co.uk>
2019-04-14 21:19:42 +0100

    Fixes curl script erroring due to lack of quotes around cache header

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 REPOSITORY_URL=https://raw.githubusercontent.com/garrym/raspberry-pi-alpine/master
 NO_CACHE_HEADER="Cache-Control: no-cache"
 
-curl -o setup-packages.sh -H $NO_CACHE_HEADER $REPOSITORY_URL/setup-packages.sh
-curl -o setup-user.sh -H $NO_CACHE_HEADER $REPOSITORY_URL/setup-user.sh
-curl -o setup-firewall.sh -H $NO_CACHE_HEADER $REPOSITORY_URL/setup-firewall.sh
-curl -o setup-ssh.sh -H $NO_CACHE_HEADER $REPOSITORY_URL/setup-ssh.sh
-curl -o setup-docker.sh -H $NO_CACHE_HEADER $REPOSITORY_URL/setup-docker.sh
+curl -o setup-packages.sh -H '$NO_CACHE_HEADER' $REPOSITORY_URL/setup-packages.sh
+curl -o setup-user.sh -H '$NO_CACHE_HEADER' $REPOSITORY_URL/setup-user.sh
+curl -o setup-firewall.sh -H '$NO_CACHE_HEADER' $REPOSITORY_URL/setup-firewall.sh
+curl -o setup-ssh.sh -H '$NO_CACHE_HEADER' $REPOSITORY_URL/setup-ssh.sh
+curl -o setup-docker.sh -H '$NO_CACHE_HEADER' $REPOSITORY_URL/setup-docker.sh
 
 chmod +x setup-packages.sh
 chmod +x setup-user.sh
